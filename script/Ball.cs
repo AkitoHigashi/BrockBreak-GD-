ec0422c455d96168d711d91b4b8aa30689455943
hiaki0617 <hiaki0617@gmail.com>
2026-06-04 08:29:13 +0900

    [add]ボールが一定即で移動するように実装した

--- script/Ball.cs
+++ script/Ball.cs
@@ -3,10 +3,18 @@ using System;
 
 public partial class Ball : RigidBody2D
 {
-	[Export] private float _ballPower = 300f;
+	[Export] private float _startPower = 300f;
+	[Export] private float _ballSpeed = 300f;
 	
 	public override void _Ready()
 	{
-		ApplyCentralImpulse(new Vector2(1f,-1f) * _ballPower);
+		ApplyCentralImpulse(new Vector2(1f,-1f) * _startPower);
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		if(LinearVelocity.Length() > 0.001f)
+		{
+		LinearVelocity = LinearVelocity.Normalized() * _ballSpeed;	
+		}
 	}
 }
